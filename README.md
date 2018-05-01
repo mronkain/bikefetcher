e4b7cfbfefcb011f3b30554e0a2e0c32cf2c2af4
mronkain <mrnk@iki.fi>
2018-05-01 11:40:09 +0300

    add porkkalankatu graphs

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Simple cgi-bin python script that outputs the current situation of free bikes at
 the Salmisaari bikestation with a moderately ugly SVG color box with the count.
 
 The script is currently running [here](https://flexer.430am.fi/cgi-bin/bikefetcher.py)
-and you can find a graph (data updated every 5 minutes) of last x days [here](https://mronkain.github.io/bikefetcher/).
+and you can find a graph (data updated every 5 minutes) [here](https://mronkain.github.io/bikefetcher/).
 
 Requires `pip install svgwrite` and `pip install requests`.
 
--- graph.js
+++ graph.js
@@ -5,9 +5,10 @@ var maekChart = function(date, data, legend) {
   fetchedSeries.push({name: legend[0].toUpperCase() + legend.slice(1), data: data});
 
   if (fetchedSeries.length == days +1) {
-  fetchedSeries.sort(function(a,b) {
-    return a["name"] > b["name"];
-  })
+    fetchedSeries.sort(function(a,b) {
+      return a["name"] > b["name"];
+    })
+
     var chart = new Chartist.Line('.chart', {
       series: fetchedSeries
     }, {
@@ -18,8 +19,9 @@ var maekChart = function(date, data, legend) {
       },
       axisX: {
         type: Chartist.FixedScaleAxis,
-        low: Number(new Date('2000-01-01 00:00')),
-        high: Number(new Date('2000-01-02 00:00')),
+        // change these if you want to show dates sequentially
+        low: Number(new Date('2006-06-06 00:00')),
+        high: Number(new Date('2006-06-07 00:00')),
         divisor: 12,
         labelInterpolationFnc: function(value) {
           return moment(value).format('HH:mm');
@@ -40,13 +42,37 @@ var fetchAndDraw = function(place, date, legend) {
     var dataset = [];
     for (var i in arr) {
       line = arr[i].split("\t");
-      dataset.push({x: new Date("2000-01-01" + " " + line[1]), y: line[2]})
+      // use date from line[0] to show days sequentially
+      dataset.push({x: new Date("2006-06-06" + " " + line[1]), y: line[2]})
     }
     maekChart(date, dataset, legend);
-  });
+  }).catch(function(error) {
+    maekChart(date, [], "-")
+});
 }
 
-fetchAndDraw("salmisaari", moment().format("YYYY-MM-DD"), "today (" + moment().subtract(i, 'days').format("dd") + ")");
-for (var i = 1; i <= days; i++) {
-  fetchAndDraw("salmisaari", moment().subtract(i, 'days').format("YYYY-MM-DD"),  "yester".repeat(i) + "day (" + moment().subtract(i, 'days').format("dd") + ")");
+var place = "both";
+
+var url = new URL(window.location);
+var p = url.searchParams.get("place");
+
+if (p == "porkkala") {
+  place = "porkkala";
+  el = document.getElementById("place");
+  el.selectedIndex = 2;
+} else if (p == "salmisaari") {
+  place = "salmisaari";
+  el = document.getElementById("place");
+  el.selectedIndex = 1;  
+} 
+
+if (place != "both") {
+  fetchAndDraw(place, moment().format("YYYY-MM-DD"), "today (" + moment().format("dd") + ")");
+  for (var i = 1; i <= days; i++) {
+    fetchAndDraw(place, moment().subtract(i, 'days').format("YYYY-MM-DD"),  "yester".repeat(i) + "day (" + moment().subtract(i, 'days').format("dd") + ")");
+  }
+} else {
+  days = 1;
+  fetchAndDraw("porkkala", moment().format("YYYY-MM-DD"), "Porkkalankatu");
+  fetchAndDraw("salmisaari", moment().format("YYYY-MM-DD"), "Salmiraarenranta");
 }

0d4dc3d12678e2b48db73b1cf9fb9a1aa8a7be65
mronkain <mrnk@iki.fi>
2018-05-03 20:01:41 +0300

    use graphql manually

--- bikefetcher.py
+++ bikefetcher.py
@@ -3,7 +3,6 @@
 import requests
 import json
 import svgwrite
-from graphqlclient import GraphQLClient
 
 def count_to_color(count):
   if count == 0:
@@ -22,41 +21,46 @@ def count_to_color(count):
 def main():
 
   try:
-    client = GraphQLClient('https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql/')
-    result = client.execute('''
+    query='''
 {
   bikeRentalStation(id:"059") { 
-    stationId
-    name
     bikesAvailable
-    spacesAvailable
   }
 }    
-    ''')
+'''
+    data = {'query': query, 'variables': None}
+    result = requests.post("https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql/", data=json.dumps(data), headers={'Accept': 'application/json', 'Content-Type': 'application/json'})
 
   except:
     print "Content-Type: text/plain;charset=utf-8"
     print
-    print "Error retrieving URL with requests package"
+    print "Request failed for reasons"
 
+  if result.status_code != 200:
     #Check status code
-  data = json.loads(result)
-  salmisaari = data['data']['bikeRentalStation'] #filter(lambda x: x["name"] == "Salmisaarenranta", stations)
-  if salmisaari:
-    count = salmisaari["bikesAvailable"]
-    
-    color = count_to_color(count)
-    dwg = svgwrite.Drawing("", (100, 100), debug=True)
-    dwg.add(dwg.rect(insert=(0, 0), size=('100%', '100%'), rx=None, ry=None, fill=color))
-    paragraph = dwg.add(dwg.g(font_size=24))
-    paragraph.add(dwg.text(count, (30, 30)))
-
-    print "Content-Type: image/svg+xml;charset=utf-8"
-    print
-    print dwg.tostring()
-  else:
     print "Content-Type: text/plain;charset=utf-8"
     print
-    print "Error: Salmisaarenranta does not exist or is too many"
+    print "Error: Status Code " + str(result.status_code)
+
+  else:
+    data = json.loads(result.content)
+    salmisaari = data['data']['bikeRentalStation'] #filter(lambda x: x["name"] == "Salmisaarenranta", stations)
+    if salmisaari:
+      count = salmisaari["bikesAvailable"]
+      
+      color = count_to_color(count)
+      dwg = svgwrite.Drawing("", (100, 100), debug=True)
+      dwg.add(dwg.rect(insert=(0, 0), size=('100%', '100%'), rx=None, ry=None, fill=color))
+      paragraph = dwg.add(dwg.g(font_size=24))
+      paragraph.add(dwg.text(count, (30, 30)))
+
+      print "Content-Type: image/svg+xml;charset=utf-8"
+      print
+      print dwg.tostring()
+    else:
+      print "Content-Type: text/plain;charset=utf-8"
+      print
+      print "Error: Salmisaarenranta not found"
+  
 
 main()
